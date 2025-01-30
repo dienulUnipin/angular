a5198ff8ecedeabd8ea98a75af15bd271662c5aa
dienulUnipin <dienul.haq@unipin.com>
2025-01-30 17:06:37 +0700

    feat: generate angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ModalComponent } from './features/modal/modal.component';
+
+export const routes: Routes = [
+    {
+        path: 'modal',
+        component: ModalComponent
+    },
+];
---
+++ src/app/features/modal/modal.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent implements OnInit{
+
+ constructor(
+  
+ ){}
+
+ public ngOnInit(): void {
+   
+ }
+}
